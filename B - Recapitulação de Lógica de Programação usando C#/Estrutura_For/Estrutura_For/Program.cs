134d3b999de67b0b08c233eb4fd277f0e50a1e62
Pedroasmaia <pedro.maia@ivoryit.com.br>
2022-12-09 19:25:34 -0300

    Exercicios

---
+++ B - Recapitulação de Lógica de Programação usando C#/Estrutura_For/Estrutura_For/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Estrutura_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+            //For (Executa a 1° vez;Condição; Executa toda vez que reinicia)
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Valor #{i}:");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
