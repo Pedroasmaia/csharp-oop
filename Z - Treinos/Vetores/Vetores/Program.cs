dc454f94d70e0b8cdd4fa548eddb0274f5825b7c
Pedroasmaia <pedro.maia@ivoryit.com.br>
2022-12-26 19:24:41 -0300

    Finalizando Modulo

---
+++ Z - Treinos/Vetores/Vetores/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = {"Pedro","Construção","Bombas" };
+            Console.WriteLine(palavras[1]);
+        }
+    }
+}
